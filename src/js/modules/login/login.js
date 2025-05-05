e680d61e574082279fdb60df222f4550e251edb1
Edwin Díaz Caballero <edwin9602@gmail.com>
2025-05-05 12:47:33 -0600

    :construction: Se realiza la validación inicial del formulario de login
    Se realiza una validación en busqueda de campos vacíos.

--- src/js/modules/login/login.js
+++ src/js/modules/login/login.js
@@ -1,8 +1,30 @@
 'use strict';
 
+// Variables
+let html = '', formData = '', requestData = '', modalId = '', modalData = '';
+
 $(async () => {
     // ========== EVENTOS ========== //
     const BODY = $('body');
 
-    // Inicio del programa
+    // Inicio de sesión
+    BODY.on('click','#btn_ingresar', function () {validarDatosInicio($(this));});
 });
+
+/**
+ * @function    validarDatosInicio
+ * @description Valida los datos de inicio de sesión de un usuario.
+ * @param {*} $btn Elemento Jquery del botón que disparó el evento 
+*/
+async function validarDatosInicio ($btn) {
+    let nameFormData = $btn.data().formulario;
+
+    // Validación de datos obligatorios
+    if(!fnWasValidate(nameFormData)) {
+        fnAlertError('Faltan campos obligatorios por llenar');
+        return;
+    }
+
+    // todo: Encriptar contraseña para enviar el dato
+    // todo: Conectar a base de datos para una validación más profunda de datos (e iniciar sesión).
+}
